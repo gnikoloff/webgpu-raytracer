2d25ca8289b3a19b0ab16874f344ced7bc5c4975
gnikoloff <nikoloffgeorgi@gmail.com>
2024-01-13 11:40:45 +0100

    change out dir in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+	base: "",
+	build: {
+		outDir: "docs",
+	},
 	plugins: [topLevelAwait()],
 	server: {
 		hmr: false,
